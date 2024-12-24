1893167f381bce80314df447d45baaec592686f0
seal256 <ilya.seashore@gmail.com>
2024-12-24 20:01:39 +0400

    adds openai call

---
+++ chatbot.py
@@ -0,0 +1,39 @@
+from openai import AsyncOpenAI
+import os
+from util import format_exception, get_logger
+from dotenv import load_dotenv
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+MODEL = 'gpt-4o-mini'
+
+global openai_client
+openai_client = AsyncOpenAI(api_key = OPENAI_API_KEY)
+log = get_logger(__name__)
+
+message_history = {}
+MAX_HISTORY_LENGTH = 50
+
+async def ask_openai(user_id: str, user_message: str):
+    try:
+        messages = message_history.get(user_id, [])
+        messages.append({"role": "user", "content": user_message})
+        response = await openai_client.chat.completions.create(
+            model=MODEL,
+            messages=messages
+        )
+        response_content = response.choices[0].message.content
+
+        if user_id not in message_history:
+            message_history[user_id] = []
+        messages.append({"role": "assistant", "content": response_content})
+        if len(messages) > MAX_HISTORY_LENGTH:
+            messages = messages[-MAX_HISTORY_LENGTH:0]
+        message_history[user_id] = messages
+        
+    except Exception as e:
+        message = format_exception(e)
+        log.error(f"ask_openai error: {message}")
+        response_content = f"ask_openai error: {message}"
+
+    return response_content
--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
 from telegram import Bot, Update
-import os
+import os, json
 from dotenv import load_dotenv
 
 from util import get_logger
-# from chatbot import ask_openai
+from chatbot import ask_openai
 
 load_dotenv()
 log = get_logger(__name__)
 
 app = FastAPI()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-# ALLOWED_USERS = [123456789] 
+ALLOWED_USERS = [7934534604] 
 bot = Bot(token=TELEGRAM_TOKEN)
 
-@app.post(f"/{TELEGRAM_TOKEN}")
+@app.post(f"/chatbot")
 async def webhook(request: Request):
-    update = Update.de_json(await request.json(), bot)
+    request_json = await request.json()
+    log.info(f"Request: {json.dumps(request_json)}")
+    update = Update.de_json(request_json, bot)
     user_id = update.message.from_user.id
 
-    # if user_id not in ALLOWED_USERS:
-    #     bot.send_message(chat_id=user_id, text="Access denied.")
-    #     return {"status": "denied"}
+    if user_id not in ALLOWED_USERS:
+        raise HTTPException(status_code=403, detail="Authentication failed")
 
     user_message = update.message.text
-    log.info(f"User {user_id} message len: {len(user_message)}")
+    log.info(f"{user_id} received message of len: {len(user_message)}")
     
-    # response = ask_openai(user_id, user_message)
-    response = f'{user_id} said: {user_message}'
+    response = await ask_openai(user_id, user_message)
     
-    chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text=response)
-    log.info(f"User {user_id} response len: {len(response)}")
+    log.info(f"{user_id} sending response of len: {len(response)}")
+
+    sent_message = await bot.send_message(chat_id=user_id, text=response)
+    log.info(f"{user_id} created message with id: {sent_message.message_id}")
 
     return {"status": "ok"}
 
